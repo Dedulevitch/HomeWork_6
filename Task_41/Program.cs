a5229774fd32af7f4299258b3b3f94f86298ce82
Anton <dedulevitchanton@gmail.com>
2022-10-12 20:28:55 +0900

    HomeWork12.10.2022

---
+++ Task_41/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+Console.Clear();
+Console.WriteLine("КАКОЕ КОЛИЧЕСТВО ЧИСЕЛ БУДЕТ ВВЕДЕНО ? ");
+int a = int.Parse(Console.ReadLine());
+
+int sum = 0;
+Console.WriteLine("ВВЕДИТЕ ЧИСЛА ");
+int[] array = new int[a];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+    if (array[i] > 0) sum += 1;
+}
+Console.WriteLine($"ВВЕДЕНО {sum} ЧИСЕЛ БОЛЬШЕ 0");
---
+++ Task_43/Program.cs
@@ -0,0 +1,57 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+
+
+Console.Clear();
+Console.WriteLine("ВВЕДИТЕ B1 ");
+double b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("ВВЕДИТЕ K1");
+double k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("ВВЕДИТЕ B2 ");
+double b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("ВВЕДИТЕ K2 ");
+double k2 = int.Parse(Console.ReadLine());
+double x = 0;
+double y = 0;
+
+if (k1 == k2)
+{
+    if (b1 == b2) Console.WriteLine("ПРЯМЫЕ СОВПАДАЮТ");
+    else Console.WriteLine("ПРЯМЫЕ ПАРАЛЕЛЬНЫ");
+}
+else 
+{
+
+x = (b2 - b1) / (k1-k2);
+y = k1 * x + b1;
+  
+Console.Write($"{x} , {y}");
+}
+ 
+
+
+
+
+
+
+
+
+/* double x = 0;
+double y = 2 * x + 3; // 5
+double y2 = -x + 6;
+Console.Write(y); */
+
+
+
+/* Сonsole.WriteLine($"{x} и {y}"); */
+
+
+// y = k * x + b  == 2 (это и есть константа b)- выражение первой прямой  ( k рост функции b константа xy - переменные)
+// x = 0
+// k - отношение от y до x  (дельта - разница = то есть если k= 3 три клетки вправо и вверх, 1,2 два вправо один вверх)
+// потом идем от b к k (любая прямая может быть задана двумя точками)
+
+// нужно найти x (y = k)
